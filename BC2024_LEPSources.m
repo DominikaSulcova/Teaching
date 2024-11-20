223d8f3970e2c8eb2fcb4baf9168063477e30b53
Dominika Sulcova <dominika.sulcova.lab@gmail.com>
2024-11-20 15:47:41 +0100

    LEPSources: Export for BESA
    - loads selected datasets
    - flips electrodes if stimulated on the left
    - exports in ASCII (.avr) format

--- BC2024_LEPSources.m
+++ BC2024_LEPSources.m
@@ -16,6 +16,7 @@
 
 %% params
 % directories
+folder.toolbox = uigetdir(pwd, 'Choose the toolbox folder');            % local folder --> MATLAB toolboxes
 folder.LEP = uigetdir(pwd, 'Choose the folder with LEP data');          % MSH D-drive folder --> processed LEPs 
 folder.output = uigetdir(pwd, 'Choose the output folder');              % output folder --> local folder with figures, output files, export files
 cd(folder.output)
@@ -25,7 +26,7 @@ study = 'LEPSources';
 output_file = sprintf('%s\\%s_output.mat', folder.output, study);
 figure_counter = 1;
 
-%% select participants 
+%% export selected datasets for BESA
 % ----- section input -----
 params.subjects = [1	2	4	7	9	10	11	14	15	16	17	18	19	22	23	24	25	26	27	28	29	30	31	32	33	34	35	43	44	45];
 params.side = {'right' 'right' 'right' 'left' 'right' 'left' 'left' 'right' 'left' 'left' ...
@@ -36,9 +37,16 @@ params.area = 'hand';
 params.block = {'b1' 'b2'};
 params.prefix = 'bl icfilt ica_all dc ep reref ds notch bandpass dc'; 
 % ------------------------- 
+% add letswave 6 to the top of search path
+addpath(genpath([folder.toolbox '\letswave 6']));
+
 % load single-trial data
 dataset = struct;
+fprintf('loading:\nsubject')
 for s = 1:length(params.subjects)
+    % provide update
+    fprintf(' %d -', s)
+
     % determine subject ID
     if params.subjects(s) < 10
         subject_ID = sprintf('S00%d', params.subjects(s));
@@ -77,46 +85,115 @@ for s = 1:length(params.subjects)
         data(end+1, :, :, :, :, :) = dataset_block(2).data(e, :, :, :, :, 1:size(data, 6));
     end
     header.datasize = size(data);
-    dataset(s).header = header;
-    dataset(s).data = data;   
+    dataset.original(s).header = header;
+    dataset.original(s).data = data;   
 end
+clear b d e s subject_ID dataset_block data2load data header  
+fprintf('\ndone.\n\n')
+
+% prepare flip dictionary
+labels = {dataset.original(1).header.chanlocs.labels};
+labels_flipped = labels;
+for i = 1:length(labels)
+    electrode_n = str2num(labels{i}(end));
+    if isempty(electrode_n)
+    else
+        if mod(electrode_n,2) == 1              % odd number --> left hemisphere                    
+            label_new = labels{i}(1:end-1);
+            label_new = [label_new num2str(electrode_n + 1)];
+            a = find(strcmpi(labels, label_new));
+            if isempty(a)
+            else
+                labels_flipped{i} = label_new;
+            end
+        else                                    % even number --> right hemisphere 
+            label_new = labels{i}(1:end-1);
+            label_new = [label_new num2str(electrode_n - 1)];
+            a = find(strcmpi(labels, label_new));
+            if isempty(a)
+            else
+                labels_flipped{i} = label_new;
+            end
+        end
+    end
+end
+labels_dict = cat(1, labels, labels_flipped)';
+
+% flip if stimulated on the left
+fprintf('flipping:\nsubject')
+for d = 1:length(dataset.original)
+    if contains(dataset.original(d).header.name, 'left')
+        fprintf(' %d -', params.subjects(d))
+        header = dataset.original(d).header;
+        data = dataset.original(d).data;
+        [dataset.flipped(d).header, dataset.flipped(d).data, ~] = RLW_flip_electrodes(header, data, labels_dict);
+    else
+        dataset.flipped(d).header = dataset.original(d).header;
+        dataset.flipped(d).data = dataset.original(d).data;
+    end
+end
+clear d i labels labels_flipped labels_dict electrode_n label_new data header
 
 % average across trials and export in ASCII format
-fprintf('exporting:\nsubject ')
-for d = 1:length(dataset)
-    % update
-    fprintf('%d ...', params.subjects(d))
+fields = fieldnames(dataset); 
+for f = 1:length(fields)
+    fprintf('exporting %s data:\nsubject ', fields{f})
 
-    % determine segment name
-    if params.subjects(d) < 10
-        segment_name = sprintf('S00%d_%s', params.subjects(d), params.side{d});
-    else
-        segment_name = sprintf('S0%d_%s', params.subjects(d), params.side{d});
+    % create a separate folder for export, if necessary
+    if exist(sprintf('%s\\export\\%s', folder.output, fields{f})) == 0
+        mkdir(sprintf('%s\\export', folder.output), fields{f})
     end
 
-    % define export name
-    export_name = sprintf('%s\\export\\%s.avr', folder.output, segment_name);
+    % get the subset
+    statement = sprintf('subset = dataset.%s;', fields{f});
+    eval(statement)
 
-    % define export headers
-    export_line{1} = sprintf('Npts= %d TSB= %d DI= %d SB= 1.000 SC= 200.0 Nchan= %d SegmentName= %s', ...
-        size(dataset(d).data, 6), dataset(d).header.xstart*1000,  dataset(d).header.xstep*1000, size(dataset(d).data, 2), segment_name);
-    export_line{2} = strjoin({dataset(d).header.chanlocs.labels}, ' ');
-
-    % average trials
-    data = squeeze(mean(dataset(d).data, 1));
-
-    % write into an ASCII file
-    fileID = fopen(export_name, 'w');
-    fprintf(fileID, '%s\n', export_line{1});  
-    fprintf(fileID, '%s\n', export_line{2});  
-    for e = 1:size(data, 1)
-        fprintf(fileID, '%f ', data(e, :));  
-        fprintf(fileID, '\n');  
+    % export individual averages
+    for d = 1:length(subset)
+        % provide update
+        fprintf('%d ...', d)
+    
+        % determine segment name
+        if params.subjects(d) < 10
+            subject_ID = sprintf('S00%d', params.subjects(d));
+        else
+            subject_ID = sprintf('S0%d', params.subjects(d));
+        end
+        if f == 1
+            segment_name = [subject_ID '_' params.side{d}];
+        elseif f == 2
+            if strcmp(params.side{d}, 'right')
+                segment_name = [subject_ID '_right'];
+            elseif strcmp(params.side{d}, 'left')
+                segment_name = [subject_ID '_right_flipped'];
+            end
+        end
+    
+        % define export name
+        export_name = sprintf('%s\\export\\%s\\%s.avr', folder.output, fields{f}, segment_name);
+    
+        % define export headers
+        export_line{1} = sprintf('Npts= %d TSB= %d DI= %d SB= 1.000 SC= 200.0 Nchan= %d SegmentName= %s', ...
+            size(subset(d).data, 6), subset(d).header.xstart*1000,  subset(d).header.xstep*1000, size(subset(d).data, 2), segment_name);
+        export_line{2} = strjoin({subset(d).header.chanlocs.labels}, ' ');
+    
+        % average trials
+        data = squeeze(mean(subset(d).data, 1));
+    
+        % write into an ASCII file
+        fileID = fopen(export_name, 'w');
+        fprintf(fileID, '%s\n', export_line{1});  
+        fprintf(fileID, '%s\n', export_line{2});  
+        for e = 1:size(data, 1)
+            fprintf(fileID, '%f ', data(e, :));  
+            fprintf(fileID, '\n');  
+        end
+        fclose(fileID); 
     end
-    fclose(fileID); 
+    fprintf('\n')
 end
-fprintf('done.\n')
+fprintf('done.\n\n')
+clear d e f fields statement subset subject_ID export_name segment_name export_line fileID data
 
 
-clear params b d e s subject_ID dataset_block data2load data header export_name segment_name export_line fileID
-
+clear params 
