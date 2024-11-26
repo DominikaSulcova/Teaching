1adfcc66640800a052d628340bc2a6468ad4242c
Dominika_MSH <dominika.sulcova@medicalschool-hamburg.de>
2024-11-26 21:57:25 +0100

    PAFPain: alpha measures per channel
    - extracts single-trial PAF and alpha amplitude
    - plot topoplots

--- Ms2024_PAFPain.m
+++ Ms2024_PAFPain.m
@@ -170,6 +170,7 @@ clear params c d p output_vars statement
 
 %% load and plot PSD
 % ----- section input -----
+params.log_val = 'off';
 params.colours = [0.9216    0.1490    0.1490;
     0.0745    0.6235    1.0000;
     1.0000    0.4784    0.8000;
@@ -211,15 +212,174 @@ plot_PSD(visual,'log_val', 'on', 'x_lim', [5, 80], ...
     'colours', params.colours(1:length(visual.y), :), 'labels', {'original spectrum',  'aperiodic component'})
 
 % save figure and update counter
-figure_counter = figure_counter + 1;
 saveas(fig, sprintf('%s\\figures\\%s_PSD_original.png', folder.output, PAFPain_info(subject_idx).ID))
+figure_counter = figure_counter + 1;
 
+% get average signals across conditions
+data_conditions{1} = []; data_conditions{2} = []; data_conditions{3} = []; data_conditions{4} = [];
+for c = 1:length(data_RSEEG(subject_idx).dataset)
+    if contains(data_RSEEG(subject_idx).dataset{c}, 'open')
+        data_conditions{1}(end + 1 : end + size(data_RSEEG(subject_idx).PSD_st(c).oscillatory, 1), :, :) = data_RSEEG(subject_idx).PSD_st(c).oscillatory;
+    elseif contains(data_RSEEG(subject_idx).dataset{c}, 'closed')
+        data_conditions{2}(end + 1 : end + size(data_RSEEG(subject_idx).PSD_st(c).oscillatory, 1), :, :) = data_RSEEG(subject_idx).PSD_st(c).oscillatory;
+    elseif contains(data_RSEEG(subject_idx).dataset{c}, sprintf('%s %s', PAFPain_info(subject_idx).area{1}, PAFPain_info(subject_idx).side{1}))
+        data_conditions{3}(end + 1 : end + size(data_RSEEG(subject_idx).PSD_st(c).oscillatory, 1), :, :) = data_RSEEG(subject_idx).PSD_st(c).oscillatory;
+    elseif contains(data_RSEEG(subject_idx).dataset{c}, sprintf('%s %s', PAFPain_info(subject_idx).area{2}, PAFPain_info(subject_idx).side{2}))
+        data_conditions{4}(end + 1 : end + size(data_RSEEG(subject_idx).PSD_st(c).oscillatory, 1), :, :) = data_RSEEG(subject_idx).PSD_st(c).oscillatory;
+    end
+end
+for d = 1:length(data_conditions)
+    visual.y{d} = squeeze(mean(data_conditions{d}, [1,2]))';
+    visual.SD{d} = squeeze(std(squeeze(mean(data_conditions{d}, 2)), 0, 1));
+end
 
-clear a b output_vars data fig screen_size
+% plot oscillatory PSD
+fig = figure(figure_counter);
+screen_size = get(0, 'ScreenSize');
+set(fig, 'Position', [screen_size(3)/4, screen_size(4)/4, screen_size(3) / 2.5, screen_size(4) / 2])
+plot_PSD(visual,'log_val', params.log_val, 'x_lim', [5, 45], 'shading', 'off', ...
+    'colours', params.colours(1:length(visual.y), :), ...
+    'labels', {'RS-EEG eyes open', 'RS-EEG eyes closed', ...
+    sprintf('%s %s', PAFPain_info(subject_idx).area{1}, PAFPain_info(subject_idx).side{1}), ...
+    sprintf('%s %s', PAFPain_info(subject_idx).area{2}, PAFPain_info(subject_idx).side{2})})
+
+% save figure and update counter
+saveas(fig, sprintf('%s\\figures\\%s_PSD_oscillatory.png', folder.output, PAFPain_info(subject_idx).ID))
+figure_counter = figure_counter + 1;
+
+% save data and continue
+PAFPain_data.PSD = data_RSEEG;
+save(output_file, 'PAFPain_data', '-append')
+clear params a b c d output_vars data visual fig screen_size
 
 %% extract alpha measures per channel 
+% ----- section input -----
+params.FOI = [7  15];
+params.measures = {'PAF' 'CAF' 'amplitude'};
+% -------------------------
+% determine frequencies of interest
+freq_idx = data_RSEEG(subject_idx).freq >= params.FOI(1) & data_RSEEG(subject_idx).freq <= params.FOI(2);
+freq_psd = data_RSEEG(subject_idx).freq(freq_idx);
 
-%% compute ICA
+% cycle through all datasets/trials/electrodes
+for a = 1:length(data_RSEEG(subject_idx).dataset)
+    for b = 1:size(data_RSEEG(subject_idx).PSD_st(a).oscillatory, 1)
+        for c = 1:size(data_RSEEG(subject_idx).PSD_st(a).oscillatory, 2)
+            % select data at frequencies of interest            
+            psd_trial = squeeze(data_RSEEG(subject_idx).PSD_st(a).oscillatory(b, c, freq_idx))';
+
+            % determine PAF and CAF
+            PAF{a}(b, c) = freq_psd(find(psd_trial == max(psd_trial)));
+
+            % determine centroid frequency
+            if min(psd_trial) < 0
+                CAF{a}(b, c) = sum(freq_psd .* (psd_trial - min(psd_trial) + 1)) / sum(psd_trial - min(psd_trial) + 1);
+            else
+                CAF{a}(b, c) = sum(freq_psd .* psd_trial) / sum(psd_trial);
+            end
+
+            % determine amplitude
+            amplitude{a}(b, c) = max(psd_trial);            
+        end
+    end
+end
+
+% split data into conditions
+labels_conditions = {'open' 'closed' ...
+    sprintf('%s %s', PAFPain_info(subject_idx).area{1}, PAFPain_info(subject_idx).side{1}) ...
+    sprintf('%s %s', PAFPain_info(subject_idx).area{2}, PAFPain_info(subject_idx).side{2})};
+PAF_conditions{1} = []; PAF_conditions{2} = []; PAF_conditions{3} = []; PAF_conditions{4} = [];
+CAF_conditions{1} = []; CAF_conditions{2} = []; CAF_conditions{3} = []; CAF_conditions{4} = [];
+amplitude_conditions{1} = []; amplitude_conditions{2} = []; amplitude_conditions{3} = []; amplitude_conditions{4} = [];
+for c = 1:length(data_RSEEG(subject_idx).dataset)
+    if contains(data_RSEEG(subject_idx).dataset{c}, labels_conditions{1})
+        PAF_conditions{1}(end + 1 : end + size(PAF{c}, 1), :) = PAF{c};
+        CAF_conditions{1}(end + 1 : end + size(CAF{c}, 1), :) = CAF{c};
+        amplitude_conditions{1}(end + 1 : end + size(amplitude{c}, 1), :) = amplitude{c};
+    elseif contains(data_RSEEG(subject_idx).dataset{c}, labels_conditions{2})
+        PAF_conditions{2}(end + 1 : end + size(PAF{c}, 1), :) = PAF{c};
+        CAF_conditions{2}(end + 1 : end + size(CAF{c}, 1), :) = CAF{c};
+        amplitude_conditions{2}(end + 1 : end + size(amplitude{c}, 1), :) = amplitude{c};
+    elseif contains(data_RSEEG(subject_idx).dataset{c}, labels_conditions{3})
+        PAF_conditions{3}(end + 1 : end + size(PAF{c}, 1), :) = PAF{c};
+        CAF_conditions{3}(end + 1 : end + size(CAF{c}, 1), :) = CAF{c};
+        amplitude_conditions{3}(end + 1 : end + size(amplitude{c}, 1), :) = amplitude{c};
+    elseif contains(data_RSEEG(subject_idx).dataset{c}, labels_conditions{4})
+        PAF_conditions{4}(end + 1 : end + size(PAF{c}, 1), :) = PAF{c};
+        CAF_conditions{4}(end + 1 : end + size(CAF{c}, 1), :) = CAF{c};
+        amplitude_conditions{4}(end + 1 : end + size(amplitude{c}, 1), :) = amplitude{c};
+    end
+end
+
+% save to measures structure
+PAFPain_measures(subject_idx).alpha.conditions = labels_conditions;
+PAFPain_measures(subject_idx).alpha.PAF = PAF_conditions;
+PAFPain_measures(subject_idx).alpha.CAF = CAF_conditions;
+PAFPain_measures(subject_idx).alpha.amplitude = amplitude_conditions;
+
+% load default header
+load('dataset_default.lw6', '-mat')
+
+% add letswave 6 to the top of search path
+addpath(genpath([folder.toolbox '\letswave 6']));
+
+% plot topoplots with mean values
+fig_CAF = figure('Position', [100, 100, 700, 550]);
+fig_amplitude = figure('Position', [150, 150, 700, 550]);
+for d = 1:length(labels_conditions)
+    % extract mean measures
+    CAF_plot = mean(CAF_conditions{d}, 1);
+    amplitude_plot = mean(amplitude_conditions{d}, 1);
+
+    % plot CAF topoplot
+    figure(fig_CAF)
+    subplot(2, 2, d)
+    plot_topo(CAF_plot, header.chanlocs, 'lims', [8 12])
+    title(labels_conditions{d})
+    if d == 1
+        sgtitle('CAF distribution')
+    end
+    hold on
+
+    % plot CAF topoplot
+    figure(fig_amplitude)
+    subplot(2, 2, d)
+    plot_topo(amplitude_plot, header.chanlocs, 'lims', [0 10])
+    if d == 1
+        sgtitle('alpha amplitude distribution')
+    end
+    title(labels_conditions{d})
+    hold on
+end
+
+% save figures
+saveas(fig_CAF, sprintf('%s\\figures\\%s_CAF_topo.png', folder.output, PAFPain_info(subject_idx).ID))
+saveas(fig_amplitude, sprintf('%s\\figures\\%s_amplitude_topo.png', folder.output, PAFPain_info(subject_idx).ID))
+
+% save output structures and continue
+save(output_file, 'PAFPain_measures', '-append')
+clear params a b c d freq_idx freq_psd psd_trial ...
+    PAF PAF_conditions CAF CAF_plot CAF_conditions amplitude amplitude_plot amplitude_conditions ...
+    fig_amplitude fig_CAF labels_conditions 
+
+%% compute ICA at alpha frequency
+% ----- section input -----
+params.prefix = '';
+params.bandpass = [8 12];
+params.ICA_comp = 7;
+% ------------------------- 
+% add letswave 7 to the top of search path
+addpath(genpath([folder.toolbox '\letswave 7']));
+
+% prepare letswave-friendly dataset
+for a = 1:length(data_RSEEG(subject_idx).dataset)
+    % prepare header
+    dataset(a).header = header;
+
+
+end
+
+clear a
 
 %% extract alpha measures per component
 
@@ -228,10 +388,10 @@ clear a b output_vars data fig screen_size
 %% functions
 function plot_PSD(visual, varargin)
 % =========================================================================
-% Reloads pre-processed EEG data of a single subject for following 
-% processing steps. 
+% Plots the PSD.  
 % Input:    - visual = a structure with x, y, SD
-%           - varargins: 'log_val', 'colours', 'x_lim', 'y_lim', 'shading', 'labels'
+%           - varargins: 
+%             'log_val', 'colours', 'x_lim', 'y_lim', 'shading', 'labels'
 % ========================================================================= 
 % set defaults
 log_val = false;
@@ -249,7 +409,7 @@ end
 if ~isempty(varargin)
     % log input values
     i = find(strcmpi(varargin, 'log_val'));
-    if ~isempty(i) strcmp(varargin{i + 1}, 'on')
+    if ~isempty(i) && strcmp(varargin{i + 1}, 'on')
         log_val = true;
     end
 
@@ -344,4 +504,34 @@ xlabel(ax_labels.x)
 ylabel(ax_labels.y)
 set(gca, 'FontSize', 14)
 set(gca, 'Layer', 'Top')
+end
+function plot_topo(data, chanlocs, varargin)
+% =========================================================================
+% Plots a topoplot.  
+% Input:    - data = a vector of measured values 1 x n of channels
+%           - chanlocs = a structure with channel locations
+%           - varargins: 
+%             'lims' 'label'
+% ========================================================================= 
+% define defaults
+lims = [-5 5];
+
+% check for varargins
+if ~isempty(varargin)
+    % limits
+    i = find(strcmpi(varargin, 'lims'));
+    if ~isempty(i) 
+        lims = varargin{i + 1};
+    end
+end
+
+% plot topography
+topoplot(data, chanlocs, 'shading', 'interp', 'whitebk', 'on', 'electrodes', 'off', 'maplimits', lims)
+set(gca,'color', [1 1 1]);
+
+% add colorbar
+colormap(jet)
+cb = colorbar;
+set(cb, 'Position', [0.15, 0.05, 0.7, 0.03]);
+set(cb, 'Orientation', 'horizontal');
 end
